8e2f760de6e6c1445878a5c3923b0f2de054f8aa
md saif ali <sali964876@gmail.com>
2024-09-03 21:24:56 +0530

    100days_of_code

---
+++ Day3/conditional_statement.py
@@ -0,0 +1,14 @@
+#   if   condition:
+#       do this...
+#   else:
+#       do this
+
+# =====================================
+
+
+print("wellcome to the rollercoster")
+height=int(input("enter your height in cm?"))
+if height >120:
+    print("you can ride the rollercoster")
+else:
+    print("sorry: you can not ride the rollercoster")
