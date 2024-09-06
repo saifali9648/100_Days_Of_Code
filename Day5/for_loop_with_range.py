a4c1c59092e27ee18e22f9d55784fda0716f896e
md saif ali <sali964876@gmail.com>
2024-09-06 17:13:14 +0530

    100days_of_code

---
+++ Day5/for_loop_with_range.py
@@ -0,0 +1,5 @@
+print("sum of 1 to 100")
+sum=0
+for i in range(1,101):
+    sum+=i
+print(sum)
---
+++ Day5/print_any_table.py
@@ -0,0 +1,4 @@
+number=int(input("Type a number that which you want the table of nunmber: "))
+for i in range(1,11):
+    table=number*i
+    print(f"{number} * {i} = {table}")
