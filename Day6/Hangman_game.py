bc88895d117338865c38f79b02fd89b6f556b3c0
md saif ali <sali964876@gmail.com>
2024-09-08 12:25:29 +0530

    100days_of_codde

--- Day6/Hangman_game.py
+++ Day6/Hangman_game.py
@@ -9,11 +9,27 @@ for j in choosen_word:
     placeholder+="_"
 print(f"{placeholder} ")
 
-guess=input("guess a letter: ").lower()
-blank_fill_word=""
-for i in choosen_word:
-    if i==guess:
-        blank_fill_word+=i
-    else:
-        blank_fill_word+="_"
-print(blank_fill_word)
+game_over=False
+correct_letter=[]
+lives=6
+while not game_over:
+    guess=input("guess a letter: ").lower()
+    blank_fill_word=""
+    for i in choosen_word:
+        if i==guess:
+            blank_fill_word+=i
+            correct_letter.append(guess)
+        elif i in correct_letter:
+            blank_fill_word+=i
+        else:
+            blank_fill_word+="_"
+
+    if guess not in choosen_word:
+        lives-=1
+        if lives==0:
+            game_over=True
+            print("you lose")
+    print(blank_fill_word)
+    if "_" not in blank_fill_word:
+        game_over=True
+        print("you win")
---
+++ Day6/append_function_check.py
@@ -0,0 +1,77 @@
+# a=5
+# while a>0:
+#     khali_list=[]
+#     guess=input("guess a letter: ")
+#     khali_list.append(guess)
+#     a-=1
+# print(khali_list)
+# import random
+
+# word_list=["saif","rashid","moni","tomato"]
+# choosen_word=random.choice(word_list)
+# print(choosen_word)
+# life=0
+# for i in choosen_word:
+#     life+=1
+# print(life)
+
+
+# stages=['''
+# ==============
+#          ___________.._______
+# | .__________))______|
+# | | / /      ||
+# | |/ /       ||
+# | | /        ||.-''.
+# | |/         |/  _  \
+# | |          ||  `/,|
+# | |          (\\`_.'
+# | |         .-`--'.
+# | |        /Y . . Y\
+# | |       // |   | \\
+# | |      //  | . |  \\
+# | |     ')   |   |   (`
+# | |          ||'||
+# | |          || ||
+# | |          || ||
+# | |          || ||
+# | |         / | | \
+# """"""""""|_`-' `-' |"""|
+# |"|"""""""\ \       '"|"|
+# | |        \ \        | |
+# : :         \ \       : :  sk
+# . .          `'       . .
+# ''']
+# print(stages)
+
+
+# stage2=['''\
+# ===============
+#     +------
+#     |     |
+#           |
+#           |
+#           |
+#           |     
+
+# ''']
+# print(stage2)
+
+print[r"""\
+
+                                   ._ o o
+                                   \_`-)|_
+                                ,""       \ 
+                              ,"  ## |   ಠ ಠ. 
+                            ," ##   ,-\__    `.
+                          ,"       /     `--._;)
+                        ,"     ## /
+                      ,"   ##    /
+
+
+                """]
+
+
+
+
+
