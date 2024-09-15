400e33e8bb7f688fb4a0f48d44f0609166a0dc7d
md saif ali <sali964876@gmail.com>
2024-09-15 22:37:08 +0530

    100days_of_code

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,2 +1,34 @@
-def additon(number1,number2):
-    return number1+number2
+calculator={
+    "+": def add(n1,n2):
+            return n1+n2
+
+def sub(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def div(n1,n2):
+    return n1/n2
+
+}
+
+# def calculator(number1,number2,operator):
+#     result=0
+#     if operator=="+":
+#         return number1 + number2
+#     elif operator=="-":
+#         return number1-number2
+#     elif operator=="*":
+#         return number1*number2
+#     elif operator=="/":
+#         return number1/number2
+#     else:
+#         print("select valid opearator and try again:")
+
+# number1=int(input("enter a number that you want to calculate:-\n"))
+# print("+\n-\n*\n/")
+# operator=input("choose a operation that you want:-\n")
+# number2=int(input("enter your second number that you want to calclulate:-"))
+# result=calculator(number1=number1,number2=number2,operator=operator)
+# print(f"{result}")
