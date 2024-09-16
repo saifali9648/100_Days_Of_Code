6b23d849a3203bc982d5143cc82bc82439ed21e3
md saif ali <sali964876@gmail.com>
2024-09-16 08:45:28 +0530

    100days_of_code

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,17 +1,21 @@
-calculator={
-    "+": def add(n1,n2):
-            return n1+n2
-
-def sub(n1,n2):
+def add(n1,n2):
+    return n1+n2
+def substract(n1,n2):
     return n1-n2
-
 def multiply(n1,n2):
     return n1*n2
-
-def div(n1,n2):
+def division(n1,n2):
     return n1/n2
 
+operation={
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": division
 }
+
+
+
 
 # def calculator(number1,number2,operator):
 #     result=0
@@ -26,9 +30,23 @@ def div(n1,n2):
 #     else:
 #         print("select valid opearator and try again:")
 
-# number1=int(input("enter a number that you want to calculate:-\n"))
-# print("+\n-\n*\n/")
-# operator=input("choose a operation that you want:-\n")
-# number2=int(input("enter your second number that you want to calclulate:-"))
-# result=calculator(number1=number1,number2=number2,operator=operator)
-# print(f"{result}")
+number1=int(input("enter a number that you want to calculate:-\n"))
+print("+\n-\n*\n/")
+operator=input("choose a operation that you want:-\n")
+number2=int(input("enter your second number that you want to calclulate:-"))
+
+result=(operation[operator](number1,number2))
+should_continiou=True
+while should_continiou:
+    re_calculation=input("Do you want to continiue with perivious calculation Type 'y' for  yes or 'n' for no:-")
+    if re_calculation=="y":
+        print("+\n-\n*\n/")
+        operator=input("choose a operation that you want:-\n")
+        number2=int(input("enter your second number that you want to calclulate:-"))
+        result=(operation[operator](result,number2))
+    if re_calculation=="n":
+        should_continiou=False
+        print(f"{result}")
+    
+
+
