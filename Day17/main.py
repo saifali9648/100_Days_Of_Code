fe045f716077466607eb5478da3b762920206633
md saif ali <sali964876@gmail.com>
2024-10-05 18:42:47 +0530

    100days_of_code

--- Day17/main.py
+++ Day17/main.py
@@ -1,9 +1,9 @@
+from quiz_brain import QuizBrain
 from question_model import Question
 from data import question_data
-from quiz_brain import Quiz_brain
 question_bank=[]
 for i in question_data:
     new_question=Question(i["text"],i["answer"])
     question_bank.append(new_question)
-quiz=Quiz_brain(question_bank)
-quiz.show_question(question_number=quiz.question_number,question_list=quiz.question_list)
+quiz=QuizBrain(question_bank)
+quiz.show_question(question_list=quiz.question_list)
--- Day17/quiz_brain.py
+++ Day17/quiz_brain.py
@@ -1,17 +1,14 @@
-class Quiz_brain():
+class QuizBrain:
     def __init__(self, question_list):
         self.question_number=0
         self.score=0
         self.question_list=question_list
         self.negative_score=0
-    def show_question(self,question_number,question_list):
+    def show_question(self,question_list):
         for current_question in question_list:
             user_answer=input(f"Q{self.question_number+1}: {current_question.text} True/false: ").lower()
-            self.current_question_anwer=current_question.answer
-            if user_answer==self.current_question_anwer:
+            if user_answer==current_question.answer:
                 print(current_question.answer)
-                # current_question=self.question_list[self.question_number]
-                # user_aswer=input(f"Q{self.question_number+1}: {current_question.text} True/false: ")
                 self.question_number+=1
                 self.score+=1
                 print(f"your score is {self.score}")
