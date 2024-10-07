9da9fec675d23474e4b56a1b5659655acbae16d9
md saif ali <sali964876@gmail.com>
2024-10-07 21:16:13 +0530

    100days_of_code

--- Day18/different_shaps2.py
+++ Day18/different_shaps2.py
@@ -4,15 +4,15 @@ from random import Random
 tim=Turtle()
 rand=Random()
 screen=Screen()
-clr=["red","green","blue","black","gray"]
+clr=["red","green","blue","black","gray","crimson","medium violet red","yellow"]
 
 def draw_shape(number_of_side,rand_color):
     angle=360/number_of_side
     for i in range(number_of_side):
         tim.color(rand_color)
-        tim.forward(30)
+        tim.forward(100)
         tim.right(angle)
-for number in range(3,10):
+for number in range(3,11):
     random_color=random.choice(clr)
     draw_shape(number,random_color)
 screen.exitonclick()
---
+++ Day18/random_walk.py
@@ -0,0 +1,19 @@
+from turtle import Turtle,Screen
+from random import Random
+tim=Turtle()
+rand=Random()
+screen=Screen()
+clor=["red","green","blue","black","gray","crimson","medium violet red","yellow"]
+directions=[0,90,180,270]
+
+def random_moves(dir,clr):
+    tim.pensize(10)
+    tim.speed(20)
+    tim.color(clr)
+    tim.forward(30)
+    tim.setheading(dir)
+
+for i in range(200):
+    random_clor=rand.choice(clor)
+    random_direction=rand.choice(directions)
+    random_moves(random_direction,random_clor)
