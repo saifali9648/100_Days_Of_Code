9069cfeb2cc668512383605049eefa9471c6bcdf
md saif ali <sali964876@gmail.com>
2024-10-09 11:39:41 +0530

    100days_of_code

--- Day18/project.py
+++ Day18/project.py
@@ -15,35 +15,30 @@ def left_to_right():
 def band_in_right_side():
     tim.right(90)
     tim.penup()
-    tim.forward(15)
+    tim.forward(30)
     tim.pendown()
     tim.right(90)
 
 def band_in_left_side():
     tim.left(90)
     tim.penup()
-    tim.forward(15)
+    tim.forward(30)
     tim.pendown()
     tim.left(90)
 
 def right_to_left():
     for j in range(3):
         for c in ("red","green","blue"):
+            tim.color(c)
+            tim.dot(15)
             tim.penup()
-            tim.forward(270)
-            tim.pendown()
-            # tim.color(c)
-            # tim.dot(15)
-            # tim.penup()
-            # tim.forward(30)
-            # tim.pendown
-
+            tim.forward(30)
+            tim.pendown
 
 for i in range(5):
     left_to_right()
     band_in_right_side()
     right_to_left()
     band_in_left_side()
-
 screen.exitonclick()
 
