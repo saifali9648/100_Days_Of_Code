f21b88e1c311d67abafab6580251e8f5eb5d88ec
md saif ali <sali964876@gmail.com>
2024-10-17 19:53:12 +0530

    100days_of_code

---
+++ Day19/higher_lower_guessing_game.py
@@ -0,0 +1,25 @@
+h_amount=0
+l_amount=0
+def show_result(higher,lower):
+    if higher>lower:
+        print(higher)
+        print("Result is : Lower")
+    elif higher<lower:
+        print(lower)
+        print("Result is : Higher")
+
+
+bet_again=True
+while bet_again:
+    print("Welcome to the Higher/Lower Guessing Game !")
+    print("You can choose Higher or Lower for your bet.")
+    choose_difficulty_level=input("Choose your bet. The 'Higher' or 'Lower':")
+    bet_amount=int(input("Enter your bet amount: "))
+    if choose_difficulty_level=="higher":
+        h_amount+=bet_amount
+    elif choose_difficulty_level=="lower":
+        l_amount+=bet_amount
+    else:
+        bet_again=False
+        show_result(h_amount,l_amount)
+        
