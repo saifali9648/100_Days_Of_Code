fb550ad74c69bea8f775b8e71136a3772ff7a6f7
md saif ali <sali964876@gmail.com>
2024-10-17 19:56:22 +0530

    100days_of_code

--- Day19/higher_lower_guessing_game.py
+++ Day19/higher_lower_guessing_game.py
@@ -15,11 +15,12 @@ while bet_again:
     print("You can choose Higher or Lower for your bet.")
     choose_difficulty_level=input("Choose your bet. The 'Higher' or 'Lower':")
     bet_amount=int(input("Enter your bet amount: "))
+    yes_no=input("do you want to more bet yes for y or no for n:")
     if choose_difficulty_level=="higher":
         h_amount+=bet_amount
     elif choose_difficulty_level=="lower":
         l_amount+=bet_amount
-    else:
+    if yes_no=="n":
         bet_again=False
         show_result(h_amount,l_amount)
         
