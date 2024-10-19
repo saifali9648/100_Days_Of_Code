33b66873ba94bf54e19d408a44f4e2071e904740
md saif ali <sali964876@gmail.com>
2024-10-19 21:31:51 +0530

    100days_of_code

---
+++ Day20_Snake_game/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle,Screen
+screen=Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game!")
+x=0
+y=0
+for i in range(0,2):
+    snake=Turtle(shape="square")
+    snake.color("red")
+    snake.goto(x=x,y=y)
+    snake.penup()
+    x-=20
+
+
+screen.exitonclick()
