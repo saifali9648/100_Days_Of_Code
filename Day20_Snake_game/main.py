826fe5c76d0dddf3910f3d12176aef78e64c4710
md saif ali <sali964876@gmail.com>
2024-10-21 21:19:53 +0530

    100days_of_code

--- Day20_Snake_game/main.py
+++ Day20_Snake_game/main.py
@@ -1,14 +1,26 @@
-from turtle import Turtle,Screen
-screen=Screen()
-screen.setup(width=600,height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game!")
-x=0
-y=0
-for i in range(0,2):
-    snake=Turtle(shape="square")
-    snake.color("red")
-    snake.goto(x=x,y=y)
-    snake.penup()
-    x-=20
-screen.exitonclick()
+# from turtle import Turtle,Screen
+# screen=Screen()
+# screen.setup(width=600,height=600)
+# screen.bgcolor("black")
+# screen.title("My Snake Game!")
+# x=0
+# y=0
+# for i in range(0,2):
+#     snake=Turtle(shape="square")
+#     snake.color("red")
+#     snake.goto(x=x,y=y)
+#     snake.penup()
+#     x-=20
+# screen.exitonclick()\
+
+
+def factorial(n):
+    # Base case
+    if n == 0:
+        return 1
+    # Recursive case
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(5))
---
+++ Day20_Snake_game/pandas_practice.py
@@ -0,0 +1,5 @@
+import pandas as pd
+pd.options.display.max_rows=9999
+df=pd.read_csv('Day20_Snake_game\data.csv')
+print(df)
+print(pd.options.display.max_rows)
